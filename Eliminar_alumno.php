048908da78bf79e384987923b415db6d974fcb42
dixtinox <102125841+dixtinox@users.noreply.github.com>
2022-05-19 09:57:37 -0500

    Update Eliminar_alumno.php

--- Eliminar_alumno.php
+++ Eliminar_alumno.php
@@ -1,77 +1,32 @@
 <html>
 
     <body>
-    
-        <style>
-        
-            html{
-                background-color: dimgrey;
-                color: aliceblue;
-            }
-            
-            div{
-                align-content: center;
-                text-align: center;
-                margin-top: 2%;
-            }
-            
-            h1{
-                font-size: 2.3vw;
-            }
-            
-            .boton{
-                align-content: center;
-                height: 5%;
-                width: 12%;
-                font-size: 1.4vw;
-                background-color: #cccccc;
-                border: none;
-                border-radius: 20px;
-            }
-            
-            .boton:hover{
-                background-color: #9c9c9c;
-            }
-            
-            .boton:active{
-                transform: scale(0.98);
-            }
-        
-        </style>
-        
-        <!--
-        <script type="text/javascript">
-            if(!confirm("¿Estás seguro de querer borrar este registro?")){
-                window.location.replace = "alumnos.php";
-            }
-        </script>
-        -->
-        
-        <?php 
+        <?php
+            include 'header.php';
             require('conexion.php');
             $No_lista=$_GET['No_lista'];
             $query="DELETE FROM alumno WHERE No_lista='$No_lista'";
             $resultado=$mysqli->query($query);
         ?>
-        
-        <div>
+        <br><br>
+        <div align="center">
         
             <?php if($resultado>0){?>
 
-            <div aling="center" >
-                <h1>Borrado con éxito</h1>
+            <div aling="center">
+                <h1 class="h1 text-success">Borrado con éxito</h1>
             </div>
 
             <?php } else {?>
 
             <div aling="center">
-                <h1>No se ha podido borrar el registro</h1>
+                <h1 class="h1 text-danger">No se ha podido borrar el registro</h1>
             </div>
 
             <?php } ?>
 
             <a href="alumnos.php">
-                <input type="submit" value="Regresar" class="boton">
+                <input type="submit" value="Regresar" class="btn btn-primary">
             </a>
         
         </div>
