59095a58e6ab5585dd1833a1e8afda683751d156
dixtinox <102125841+dixtinox@users.noreply.github.com>
2022-05-18 22:38:49 -0500

    Update modificacion_trabajo.php

--- modificacion_trabajo.php
+++ modificacion_trabajo.php
@@ -10,4 +10,21 @@
     $res=mysqli_query($conexion,$query);
 
     echo("<h1>Modificación Realizada con exito</h1>");
-    echo("<a href='mostrar_trabajos.php'><input type='button' value='Continuar'></a>");
+    echo("<a href='mostrar_trabajos.php'><input type='button' value='Continuar'></a>");<?php
+    include 'header.php';
+    $id=$_GET["ID"];
+    $nombre=$_GET["nombre"];
+    $materia=$_REQUEST["materia"];
+    $valor=$_GET["ponderacion"];
+    $fecha=$_GET["fecha"];
+
+    $conexion=mysqli_connect("localhost","root","","school");
+    $query="UPDATE trabajo SET Nom_trabajo='$nombre', materia_ID_materia=$materia, Ponderacion=$valor, Fecha='$fecha' WHERE idTrabajo=$id";
+    $res=mysqli_query($conexion,$query);
+
+    echo("
+    <div align='center'>
+        <h1 class='h1 display-2'>Modificación Realizada con exito</h1>
+        <a href='mostrar_trabajos.php'><input type='button' value='Continuar' class='btn btn-primary'></a>
+    </div>
+    ");
