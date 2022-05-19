2512d7eee5aeb7dc9c166b44ae01012be16cea08
dixtinox <102125841+dixtinox@users.noreply.github.com>
2022-05-19 09:59:46 -0500

    Update Modificar_alumno.php

--- Modificar_alumno.php
+++ Modificar_alumno.php
@@ -1,5 +1,5 @@
 <?php
-
+    include 'header.php';
 	require('conexion.php');	
 	$No_lista=$_GET['No_lista'];
     $id=$No_lista;
@@ -14,116 +14,64 @@
 <html>
 
     <body>
-    
-        <style>
-        
-            html{
-                background-color: dimgrey;
-                color: aliceblue;
-            }
-            
-            div{
-                align-content: center;
-                text-align: center;
-                margin: 2% 3%;
-                
-            }
-            
-            h1{
-                font-size: 2.3vw;
-            }
-            
-            .boton{
-                align-content: center;
-                height: 7%;
-                width: 20%;
-                font-size: 1.5vw;
-                background-color: #cccccc;
-                border: none;
-                border-radius: 20px;
-            }
-            
-            .boton:hover{
-                background-color: #9c9c9c;
-            }
-            
-            .boton:active{
-                transform: scale(0.98);
-            }
-            
-            .texto{
-                align-content: center;
-                width: 50%;
-                height: 8%;
-                font-size: 1.4vw;
-                text-align: left;
-                border: none;
-                border-radius: 6px;
-                padding-left: 1%;
-            }
-            
-            p{
-                font-size: 1.6vw;
-            }
-        
-        </style>
-        
-        <div align="center">
-            
-            <form method="get" action="Modi_alumno.php">
-                <table width="100%">
-                    
-                    <!-- Para poder pasar el numero de lista antiguo y evitar errores de variables-->
-                    <input type="hidden" name="id" value="<?php echo $id; ?>">
-                    
-                    <tr>
-                        <td width="30%" style="text-align: right;">
-                            <p>Número de lista: </p>
-                        </td>
-                        <td width="70%" style="content-align: left;">
-                            <input type="text" name="No_lista" class="texto" value="<?php echo $row['No_lista']; ?>">
-                        </td>
-                    </tr>
-                    
-                    <tr>
-                        <td width="30%" style="text-align: right;">
-                            <p>Nombre: </p>
-                        </td>
-                        <td width="70%" style="content-align: left;">
-                            <input type="text" name="Nombre" class="texto" value="<?php echo $row['Nombre']; ?>">
-                        </td>
-                    </tr>
-                    
-                    <tr>
-                        <td width="30%" style="text-align: right;">
-                            <p>Apellido paterno: </p>
-                        </td>
-                        <td width="70%" style="content-align: left;">
-                            <input type="text" name="Apellido_pat" class="texto" value="<?php echo $row['Apellido_pat']; ?>">
-                        </td>
-                    </tr>
-                    
-                    <tr>
-                        <td width="30%" style="text-align: right;">
-                            <p>Apellido materno: </p>
-                        </td>
-                        <td width="70%" style="content-align: left;">
-                            <input type="text" name="Apellido_mat" class="texto" value="<?php echo $row['Apellido_mat']; ?>">
-                        </td>
-                    </tr>
-                
-                </table>
-                <br>
-                <input type="submit" value="Guardar cambios" class="boton">
-                <br><br>
-                <a href="alumnos.php">
-                    <input type="button" value="Regresar" class="boton">
-                </a>
-                
-            </form>
-            
+    <div class="row">
+        <div class="col-md-4"></div>
+        <div align="center" class="col-md-4 p-1">
+            <div class="card p-2">
+                <div class="card-header">Datos</div>
+                <form method="get" action="Modi_alumno.php" >
+                    <table>
+                        <!-- Para poder pasar el numero de lista antiguo y evitar errores de variables-->
+                        <input type="hidden" name="id" value="<?php echo $id; ?>">
+                        <tr>
+                            <td>
+                                <p>Número de lista: </p>
+                            </td>
+                            <td>
+                                <input type="text" name="No_lista" class="texto" value="<?php echo $row['No_lista']; ?>">
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>
+                                <p>Nombre: </p>
+                            </td>
+                            <td>
+                                <input type="text" name="Nombre" class="texto" value="<?php echo $row['Nombre']; ?>">
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>
+                                <p>Apellido paterno: </p>
+                            </td>
+                            <td>
+                                <input type="text" name="Apellido_pat" class="texto" value="<?php echo $row['Apellido_pat']; ?>">
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>
+                                <p>Apellido materno: </p>
+                            </td>
+                            <td>
+                                <input type="text" name="Apellido_mat" value="<?php echo $row['Apellido_mat']; ?>">
+                            </td>
+                        </tr>
+
+                    </table>
+                    <br>
+                    <input type="submit" value="Guardar cambios" class="btn btn-success">
+                    <br><br>
+                    <a href="alumnos.php">
+                        <input type="button" value="Regresar" class="btn btn-danger">
+                    </a>
+
+                </form>
+
+            </div>
         </div>
-    
+    </div>
     </body>
 
 </html>
