df83e2668501f4a193c6840daf99d1c1ac416787
dixtinox <102125841+dixtinox@users.noreply.github.com>
2022-05-18 22:40:52 -0500

    Update eliminar_trabajo.php

--- eliminar_trabajo.php
+++ eliminar_trabajo.php
@@ -4,4 +4,15 @@
     $query="DELETE FROM trabajo WHERE idTrabajo=$id";
     $res=mysqli_query($conexion,$query);
     echo("<h1>Trabajo borrado</h1>");
-    echo("<a href='mostrar_trabajos.php'><input type='button' value='Continuar'></a>");
+    echo("<a href='mostrar_trabajos.php'><input type='button' value='Continuar'></a>");<?php
+    include 'header.php';
+    $id=$_GET["ID"];
+    $conexion=mysqli_connect("localhost","root","","school");
+    $query="DELETE FROM trabajo WHERE idTrabajo=$id";
+    $res=mysqli_query($conexion,$query);
+    echo("
+    <div align='center'>
+        <h1 class='h1 display-2'>Trabajo borrado</h1>
+        <a href='mostrar_trabajos.php'><input type='button' value='Continuar' class='btn btn-primary'></a>
+    </div>
+    ");
