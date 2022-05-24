d4c4e4098b599fb58589fc882fdd2ea685d001a9
dixtinox <102125841+dixtinox@users.noreply.github.com>
2022-05-23 23:47:53 -0500

    Update eliminar_trabajo.php

--- eliminar_trabajo.php
+++ eliminar_trabajo.php
@@ -2,6 +2,8 @@
     include 'header.php';
     $id=$_GET["ID"];
     $conexion=mysqli_connect("localhost","root","","school");
+    $q2="DELETE FROM evidencia WHERE Trabajo_idTrabajo=$id";
+    $res2=mysqli_query($conexion, $q2);
     $query="DELETE FROM trabajo WHERE idTrabajo=$id";
     $res=mysqli_query($conexion,$query);
     echo("
