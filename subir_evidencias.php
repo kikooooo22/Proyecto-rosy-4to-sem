6991b74a01cae2a991a5d56dd320f7d214e61270
dixtinox <102125841+dixtinox@users.noreply.github.com>
2022-05-23 21:34:23 -0500

    Add files via upload

---
+++ subir_evidencias.php
@@ -0,0 +1,58 @@
+<html>
+    <head>
+        <?php
+            include 'header.php';
+            $conexion=mysqli_connect("localhost","root","","school");
+            $queryT="SELECT * FROM trabajo";
+            $res=mysqli_query($conexion,$queryT);
+            $queryE="SELECT * FROM evidencia";
+            $res2=mysqli_query($conexion, $queryE);
+        ?>
+    </head>
+    <body>
+        <br>
+        <div class="container" align="middle">
+            <form method="get">
+                <div class="form-group">
+                    <label class="text-dark">Elige un trabajo:</label><br>
+                    <select name="trabajo">
+                        <option value="">Elige una opción</option>
+                        <?php
+                            while($row=mysqli_fetch_array($res)){
+                                ?>
+                        <option value=<?php $temp=$row['idTrabajo']?>><?php echo($row['Nom_trabajo'])?></option>
+                        <?php } ?>
+                    </select>
+                    <input type="submit" class="btn btn-primary" value="Elegir">
+                </div>
+            </form>
+        </div>
+        <br>
+        <div class="container">
+            <table class="table table-bordered">
+                <thead class="table-dark">
+                <tr>
+                    <td>ID Trabajo</td>
+                    <td>No. Lista</td>
+                    <td>Calificacion</td>
+                    <td>Subir</td>
+                </tr>
+                </thead>
+                <tbody>
+                <?php while($reg=mysqli_fetch_array($res2)){?>
+                    <tr>
+                        <td><?php echo($reg['Trabajo_idTrabajo']) ?></td>
+                        <td><?php echo($reg['alumno_No_lista'])?></td>
+                        <td><?php echo($reg['Calf_trabajo'])?></td>
+                        <td>
+                            <a>
+                                <input type="submit" class="btn btn-primary" value="subir">
+                            </a>
+                        </td>
+                    </tr>
+                <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </body>
+</html>
